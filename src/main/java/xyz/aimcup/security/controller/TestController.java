37f336fcd53b3e56e4e07cd49351ae35ba05d4e0
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-07-11 21:27:51 +0200

    created testcontroller for pr testing purpose

---
+++ src/main/java/xyz/aimcup/security/controller/TestController.java
@@ -0,0 +1,19 @@
+package xyz.aimcup.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String test() {
+        // this comment should be pointed by pmd
+        return "test";
+    }
+}
