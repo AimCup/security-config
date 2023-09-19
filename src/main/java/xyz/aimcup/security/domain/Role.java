24026cc73dec7010cff48b5b7b2d1fd1971dd8f4
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-09-19 19:06:08 +0200

    AC-7 Fixed schema exception on column 'name'

--- src/main/java/xyz/aimcup/security/domain/Role.java
+++ src/main/java/xyz/aimcup/security/domain/Role.java
@@ -1,12 +1,16 @@
 package xyz.aimcup.security.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
 import org.springframework.security.core.GrantedAuthority;
 
 
@@ -17,6 +21,10 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60, name = "name")
     private RoleName name;
 
     public Role() {
